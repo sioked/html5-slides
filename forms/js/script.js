17d19794dbc7f1ce817caf5acb94d69c179e8e13
Ed Siok <ed@edsiok.com>
2010-09-27 22:31:13 -0500

    Updates to slides for site markup & some design changes

--- forms/js/script.js
+++ forms/js/script.js
@@ -2,67 +2,45 @@
 A simple forms example
 */
 
-$(document).ready(function(){
-	if(document.getElementById("edit_link").contentEditable != null){
-		$("#edit_link").hide();
-	
-		var value;
-		$('[contenteditable=true]').each(function(){
-			if( value = window.localStorage.getItem($(this).attr('id')) )
-			{
-				$(this).text(value);
-			}
-		});
-		
-		$('[contenteditable=true]').blur(function(){
-			window.localStorage.setItem($(this).attr('id'), $(this).text());
-			window.localStorage.setItem('timestamp', (new Date()).getTime());
-			updateLog(true);
-		});
-		
-		var updateLog = function(isNewSave){
-			var delta=0;
-			var log = document.querySelector('#log');
-			
-			if (window.localStorage.getItem('timestamp')) {
-				log.setAttribute('style', 'display: block;');
-				delta = ((new Date()).getTime() - (new Date()).setTime(window.localStorage.getItem('timestamp'))) / 1000;
-			  if (isNewSave) {
-			    log.textContent = 'Saved. Content will be available after browser refresh/reopen.';
-			    setTimeout(function() {
-			      log.textContent = '';
-			    }, 3000);
-			  } else {
-			    log.textContent = 'last saved: ' + delta + 's ago';
-			  }
-			}
-		}
-		
-		updateLog(false);
-	}
-	
-	
+$(document).ready(function() {
+
+    if (document.getElementById("edit_link").contentEditable != null) {
+        $("#edit_link").hide();
+        var value;
+        $('[contenteditable=true]').each(function() {
+            if (value = window.localStorage.getItem($(this).attr('id')))
+            {
+                $(this).text(value);
+            }
+        });
+
+        $('[contenteditable=true]').blur(function() {
+            window.localStorage.setItem($(this).attr('id'), $(this).text());
+            window.localStorage.setItem('timestamp', (new Date()).getTime());
+            updateLog(true);
+        });
+
+        var updateLog = function(isNewSave) {
+            var delta = 0;
+            var log = document.querySelector('#log');
+
+            if (window.localStorage.getItem('timestamp')) {
+                log.setAttribute('style', 'display: block;');
+                delta = ((new Date()).getTime() - (new Date()).setTime(window.localStorage.getItem('timestamp'))) / 1000;
+                if (isNewSave) {
+                    log.textContent = 'Saved. Content will be available after browser refresh/reopen.';
+                    setTimeout(function() {
+                        log.textContent = '';
+                    },
+                    3000);
+                } else {
+                    log.textContent = 'last saved: ' + delta + 's ago';
+                }
+            }
+        }
+
+        updateLog(false);
+    }
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
